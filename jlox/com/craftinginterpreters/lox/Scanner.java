7860e68772064449b5d5d7521033c2dfda2e5c07
josevbrito <josevbrito.tech@gmail.com>
2025-11-03 16:38:32 -0300

    feat(scanner): Implementa reconhecimento de identificadores e palavras-chave

--- jlox/com/craftinginterpreters/lox/Scanner.java
+++ jlox/com/craftinginterpreters/lox/Scanner.java
@@ -16,6 +16,28 @@ class Scanner {
     private int current = 0;
     private int line = 1;
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    AND);
+        keywords.put("class",  CLASS);
+        keywords.put("else",   ELSE);
+        keywords.put("false",  FALSE);
+        keywords.put("for",    FOR);
+        keywords.put("fun",    FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
+
     Scanner(String source) {
         this.source = source;
     }
@@ -62,6 +84,16 @@ class Scanner {
         return c >= '0' && c <= '9';
     }
 
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+            (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
     private void string() {
         while (peek() != '"' && !isAtEnd()) {
             if (peek() == '\n') line++; // Suporte para strings multilinha
@@ -94,6 +126,17 @@ class Scanner {
 
         addToken(NUMBER,
             Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        // Verifica se é uma palavra-chave
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+
+        addToken(type);
     }
 
     private void addToken(TokenType type) {
@@ -159,9 +202,10 @@ class Scanner {
             break;
 
             default:
-                // Verifica se é um dígito
                 if (isDigit(c)) {
                     number();
+                } else if (isAlpha(c)) {
+                    identifier();
                 } else {
                     Lox.error(line, "Caractere Inesperado.");
                 }
