adaf6adeb1704aba53337d0bcc5fc34941cb9902
iaraloayza <anaiaraloayza11@gmail.com>
2025-11-23 18:54:41 -0300

    feat(interpreter): Implementa operadores binários, aritmética e comparações

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -12,6 +12,14 @@ class Interpreter implements Expr.Visitor<Object> {
         return true;
     }
 
+    // Verifica se dois objetos são iguais.
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null) return true;
+        if (a == null) return false;
+        
+        return a.equals(b);
+    }
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
@@ -39,6 +47,42 @@ class Interpreter implements Expr.Visitor<Object> {
 
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right); 
+
+        switch (expr.operator.type) {
+            case GREATER:
+                return (double)left > (double)right;
+            case GREATER_EQUAL:
+                return (double)left >= (double)right;
+            case LESS:
+                return (double)left < (double)right;
+            case LESS_EQUAL:
+                return (double)left <= (double)right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                // O operador + é especial: soma números OU concatena strings.
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                } 
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+                
+                break;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        // Inalcançável.
         return null;
     }
 }
